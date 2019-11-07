15bad0e892b57c60cac39e9e054f00d9723ccccd
Sergey Ukolov <zezic51@yandex.ru>
2019-11-07 20:44:57 +0300

    Allow to select between +/- 5V and +/- 10V range for SH-8. Related to #52.

--- src/SH-8.cpp
+++ src/SH-8.cpp
@@ -22,26 +22,43 @@ struct SH8 : Module {
   float last_trig_inputs[NUM_CHANNELS];
   dsp::SchmittTrigger triggers[NUM_CHANNELS];
 
+  /* Settings */
+  bool wideRange = false;
+
   SH8() {
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
   }
   void process(const ProcessArgs &args) override;
+
+  json_t *dataToJson() override {
+    json_t *rootJ = json_object();
+    json_object_set_new(rootJ, "wideRange", json_boolean(wideRange));
+    return rootJ;
+  }
+
+  void dataFromJson(json_t *rootJ) override {
+    json_t *wideRangeJ = json_object_get(rootJ, "wideRange");
+    if (wideRangeJ) { wideRange = json_boolean_value(wideRangeJ); }
+  }
 };
 
 
 void SH8::process(const ProcessArgs &args) {
+  float multiplier = wideRange ? 4.f : 2.f;
   bool previousIsTriggered = false;
   for (int i = 0; i < NUM_CHANNELS; i++) {
     if (inputs[TRIG_INPUT + i].isConnected()) {
       if (triggers[i].process(inputs[TRIG_INPUT + i].getVoltage())) {
-        outputs[HOLD_OUTPUT + i].setVoltage(inputs[NOISE_INPUT].isConnected() ? inputs[NOISE_INPUT].getVoltage() : random::normal() * 2.0);
+        outputs[HOLD_OUTPUT + i].setVoltage(
+          clamp(inputs[NOISE_INPUT].isConnected() ? inputs[NOISE_INPUT].getVoltage() : random::normal() * multiplier, -10.f, 10.f));
         previousIsTriggered = true;
       } else {
         previousIsTriggered = false;
       }
     } else {
       if (i > 0 && previousIsTriggered) {
-        outputs[HOLD_OUTPUT + i].setVoltage(inputs[NOISE_INPUT].isConnected() ? inputs[NOISE_INPUT].getVoltage() : random::normal() * 2.0);
+        outputs[HOLD_OUTPUT + i].setVoltage(
+          clamp(inputs[NOISE_INPUT].isConnected() ? inputs[NOISE_INPUT].getVoltage() : random::normal() * multiplier, -10.f, 10.f));
       }
     }
   }
@@ -68,7 +85,44 @@ struct SH8Widget : ModuleWidget {
     addChild(createWidget<ZZC_Screw>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(createWidget<ZZC_Screw>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
   }
+  void appendContextMenu(Menu *menu) override;
 };
 
+struct SH810VRangeItem : MenuItem {
+  SH8 *sh8;
+  void onAction(const event::Action &e) override {
+    sh8->wideRange = false;
+  }
+  void step() override {
+    rightText = CHECKMARK(!sh8->wideRange);
+  }
+};
+
+struct SH820VRangeItem : MenuItem {
+  SH8 *sh8;
+  void onAction(const event::Action &e) override {
+    sh8->wideRange = true;
+  }
+  void step() override {
+    rightText = CHECKMARK(sh8->wideRange);
+  }
+};
+
+void SH8Widget::appendContextMenu(Menu *menu) {
+  menu->addChild(new MenuSeparator());
+
+  SH8 *sh8 = dynamic_cast<SH8*>(module);
+  assert(sh8);
+
+  menu->addChild(construct<MenuLabel>(&MenuLabel::text, "Range"));
+
+  SH810VRangeItem * sh810VRangeItem = createMenuItem<SH810VRangeItem>("+/- 5V");
+  sh810VRangeItem->sh8 = sh8;
+  menu->addChild(sh810VRangeItem);
+
+  SH820VRangeItem * sh820VRangeItem = createMenuItem<SH820VRangeItem>("+/- 10V");
+  sh820VRangeItem->sh8 = sh8;
+  menu->addChild(sh820VRangeItem);
+}
 
 Model *modelSH8 = createModel<SH8, SH8Widget>("SH-8");
