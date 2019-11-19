f8a5e98e3d9fd1e1282476ecb42de8414946d6d8
Sergey Ukolov <zezic51@yandex.ru>
2019-11-19 21:59:19 +0300

    Bump version. Add crossrelease script.

---
+++ crossrelease.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import json
+import os
+import sys
+import timeit
+
+OS_OVERRIDES = {
+    'macos': {
+        'CC': 'x86_64-apple-darwin19-clang',
+        'CXX': 'x86_64-apple-darwin19-clang++-libc++',
+        'STRIP': 'x86_64-apple-darwin19-strip'
+    },
+    'windows': {
+        'CC': 'x86_64-w64-mingw32-gcc',
+        'CXX': 'x86_64-w64-mingw32-g++',
+        'STRIP': 'x86_64-w64-mingw32-strip'
+    }
+}
+
+TARGETS = ['Linux', 'macOS', 'Windows']
+SHORTLANDS = {
+    'Linux': 'lin',
+    'macOS': 'mac',
+    'Windows': 'win'
+}
+
+
+class SomethingWentWrong(Exception):
+    pass
+
+
+def do(command):
+    #  os.system(command)
+    #  return
+    code = os.system('{} >/dev/null 2>&1'.format(command))
+    if code:
+        raise SomethingWentWrong()
+
+
+def build(target):
+    overrides = OS_OVERRIDES.get(target, {})
+    for key, value in overrides.items():
+        os.environ[key] = value
+    do('make clean')
+    do('make -j{} dist'.format(os.cpu_count()))
+
+
+with open('plugin.json', 'r') as fd:
+    manifest = json.load(fd)
+
+slug = manifest.get('slug')
+version = manifest.get('version')
+
+destination = '{}-{}'.format(slug, version)
+os.makedirs(destination, exist_ok=True)
+
+total_time = 0
+
+for target in TARGETS:
+    print('Building {} for {}...'.format(slug, target), end=' ')
+    sys.stdout.flush()
+    start = timeit.default_timer()
+    try:
+        build(target.lower())
+    except SomethingWentWrong:
+        print('Something went wrong when building {} for {}!'.format(slug, target))
+        sys.exit(1)
+    archive_name = '{}-{}-{}.zip'.format(
+        slug,
+        version,
+        SHORTLANDS.get(target)
+    )
+    os.rename(
+        'dist/{}'.format(archive_name),
+        '{}/{}'.format(destination, archive_name)
+    )
+    end = timeit.default_timer()
+    elapsed = end - start
+    total_time += elapsed
+    print('{}s'.format(round(elapsed, 1)))
+
+print('Total time: {}s'.format(round(total_time, 1)))
+
+print('Done! Release files are in {} folder.'.format(destination))
+print('')
