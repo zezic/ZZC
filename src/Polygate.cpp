f1ca91e17149a8fc9b8952507d0bb3460fa1c6c5
Sergey Ukolov <zezic51@yandex.ru>
2019-11-20 00:15:00 +0300

    Smart mode and momentary mode for Polydiv.

--- src/Polygate.cpp
+++ src/Polygate.cpp
@@ -2,6 +2,36 @@
 
 const int NUM_CHANNELS = 16;
 
+struct SmartTrigger {
+  bool state = false;
+  float holdTime = 0.f;
+
+  void init() {
+    this->state = false;
+    this->holdTime = 0.f;
+  }
+
+  void process(float value, float sampleTime) {
+    if (value > 0.5f) {
+      if (this->holdTime == 0.f) {
+        this->state ^= true;
+      }
+      this->holdTime += sampleTime;
+    } else {
+      if (this->holdTime > 0.35f) {
+        this->state ^= true;
+      }
+      this->holdTime = 0.f;
+    }
+  }
+};
+
+enum PolygateModes {
+  SMART_MODE,
+  MOMENTARY_MODE,
+  NUM_MODES
+};
+
 struct Polygate : Module {
   enum ParamIds {
     ENUMS(GATE_PARAM, NUM_CHANNELS),
@@ -19,12 +49,13 @@ struct Polygate : Module {
     NUM_LIGHTS
   };
 
-  dsp::BooleanTrigger triggers[NUM_CHANNELS];
-  bool gates[NUM_CHANNELS] = { false };
+  SmartTrigger triggers[NUM_CHANNELS];
 
   /* Settings */
   float range = 5.f;
   bool invertOutput = false;
+  PolygateModes mode = SMART_MODE;
+  PolygateModes lastMode = SMART_MODE;
 
   Polygate() {
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
@@ -36,13 +67,13 @@ struct Polygate : Module {
 
   void onReset() override {
     for (int i = 0; i < NUM_CHANNELS; i++) {
-      gates[i] = false;
+      this->triggers[i].state = false;
     }
   }
 
   void onRandomize() override {
     for (int i = 0; i < NUM_CHANNELS; i++) {
-      gates[i] = random::normal() > 0.5 ? true : false;
+      this->triggers[i].state = random::normal() > 0.5 ? true : false;
     }
   }
 
@@ -50,11 +81,12 @@ struct Polygate : Module {
     json_t *rootJ = json_object();
     json_t *gatesArray = json_array();
     for (int i = 0; i < NUM_CHANNELS; i++) {
-      json_array_append(gatesArray, json_boolean(gates[i]));
+      json_array_append(gatesArray, json_boolean(this->triggers[i].state));
     }
     json_object_set_new(rootJ, "gates", gatesArray);
     json_object_set_new(rootJ, "range", json_real(range));
     json_object_set_new(rootJ, "invertOutput", json_boolean(invertOutput));
+    json_object_set_new(rootJ, "mode", json_integer(mode));
     return rootJ;
   }
 
@@ -62,29 +94,50 @@ struct Polygate : Module {
     json_t *gatesArray = json_object_get(rootJ, "gates");
     json_t *rangeJ = json_object_get(rootJ, "range");
     json_t *invertOutputJ = json_object_get(rootJ, "invertOutput");
+    json_t *modeJ = json_object_get(rootJ, "mode");
     if (gatesArray) {
       size_t size = json_array_size(gatesArray);
       for (size_t i = 0; i < size; i++) {
-        gates[i] = json_boolean_value(json_array_get(gatesArray, i));
+        triggers[i].state = json_boolean_value(json_array_get(gatesArray, i));
       }
     }
     if (rangeJ) { range = json_real_value(rangeJ); }
     if (invertOutputJ) { invertOutput = json_boolean_value(invertOutputJ); }
+    if (modeJ) {
+      mode = PolygateModes(json_integer_value(modeJ));
+      lastMode = mode;
+    }
   }
 };
 
 void Polygate::process(const ProcessArgs &args) {
   outputs[GATE_OUTPUT].setChannels(NUM_CHANNELS);
 
-  for (int c = 0; c < NUM_CHANNELS; c++) {
-    if (triggers[c].process(params[GATE_PARAM + c].getValue())) {
-      gates[c] ^= true;
+  if (this->mode == SMART_MODE) {
+    if (this->lastMode == MOMENTARY_MODE) {
+      for (int c = 0; c < NUM_CHANNELS; c++) {
+        triggers[c].init();
+      }
     }
-    outputs[GATE_OUTPUT].setVoltage(gates[c] ^ this->invertOutput ? range : 0.f, c);
-    if (gates[c]) {
-      lights[GATE_LED + c].value = 1.1f;
+    for (int c = 0; c < NUM_CHANNELS; c++) {
+      triggers[c].process(params[GATE_PARAM + c].getValue(), args.sampleTime);
+      outputs[GATE_OUTPUT].setVoltage(triggers[c].state ^ this->invertOutput ? range : 0.f, c);
+      if (triggers[c].state) {
+        lights[GATE_LED + c].value = 1.1f;
+      }
     }
+  } else if (this->mode == MOMENTARY_MODE) {
+    for (int c = 0; c < NUM_CHANNELS; c++) {
+      bool pressed = params[GATE_PARAM + c].getValue() > 0.5f;
+      outputs[GATE_OUTPUT].setVoltage(pressed ^ this->invertOutput ? range : 0.f, c);
+      if (pressed) {
+        lights[GATE_LED + c].value = 1.1f;
+      }
+    }
+  } else {
+    // Should not happen
   }
+  this->lastMode = this->mode;
 }
 
 struct PolygateWidget : ModuleWidget {
@@ -134,6 +187,17 @@ struct PolygateRangeItem : MenuItem {
   }
 };
 
+struct PolygateModeItem : MenuItem {
+  Polygate *polygate;
+  PolygateModes target;
+  void onAction(const event::Action &e) override {
+    polygate->mode = this->target;
+  }
+  void step() override {
+    rightText = CHECKMARK(polygate->mode == this->target);
+  }
+};
+
 void PolygateWidget::appendContextMenu(Menu *menu) {
   menu->addChild(new MenuSeparator());
 
@@ -154,6 +218,20 @@ void PolygateWidget::appendContextMenu(Menu *menu) {
     PolygateRangeItem *item = new PolygateRangeItem;
     item->text = "+" + std::to_string((int) range) + "V";
     item->target = range;
+    item->polygate = polygate;
+    menu->addChild(item);
+  }
+  menu->addChild(new MenuSeparator());
+
+  menu->addChild(construct<MenuLabel>(&MenuLabel::text, "Button Mode"));
+  std::vector<std::string> polygateModeNames = {
+    "Smart",
+    "Momentary (Hold)"
+  };
+  for (unsigned int i = 0; i < polygateModeNames.size(); i++) {
+    PolygateModeItem *item = new PolygateModeItem;
+    item->text = polygateModeNames.at(i);
+    item->target = PolygateModes(i);
     item->polygate = polygate;
     menu->addChild(item);
   }
